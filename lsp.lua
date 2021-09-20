2c2724f78f5bc0ba614d7f5170c45c763e370f85
Marc Trösken <marc.troesken@ratepay.com>
2021-09-20 15:53:05 +0200

    Initial commit

---
+++ lsp.lua
@@ -0,0 +1,11 @@
+require'lspconfig'.dockerls.setup{}
+require'lspconfig'.tsserver.setup{}
+require'lspconfig'.intelephense.setup{}
+require'lspconfig'.bashls.setup{}
+require'lspconfig'.terraformls.setup{}
+
+require('lualine').setup {
+  options = {
+    theme = 'github',
+  }
+}
