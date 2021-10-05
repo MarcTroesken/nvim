77ed3928e61b34d781ecada984855b13bc43b84d
MarcTroesken <m.troesken@mail.de>
2021-10-05 20:53:09 +0200

    Update nvim config

--- lsp.lua
+++ lsp.lua
@@ -3,6 +3,9 @@ require'lspconfig'.tsserver.setup{}
 require'lspconfig'.intelephense.setup{}
 require'lspconfig'.bashls.setup{}
 require'lspconfig'.terraformls.setup{}
+require'lspconfig'.tailwindcss.setup{}
+require'lspconfig'.vuels.setup{}
+require'nvim-tree'.setup()
 
 require('lualine').setup {
   options = {
