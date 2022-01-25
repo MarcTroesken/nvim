421ba04d10f35b8937e8aa032e3dd9bf09bbadb8
Marc Trösken <marc.troesken@ratepay.com>
2022-01-25 15:49:51 +0100

    clean that shit up and use lua

---
+++ init.lua
@@ -0,0 +1,11 @@
+require("packer-config")
+require("options")
+require("keymap-config")
+require("autopairs-config")
+require("lualine-config")
+require("colors-config.nightfox")
+require("nvim-tree-config")
+require("treesitter-config")
+require("cmp-config")
+--> lsp modules
+require("lsp-config")
---
+++ lua/keymap-config/init.lua
@@ -0,0 +1,33 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Custom
+keymap("n" ,"<C-p>", ":Telescope find_files<cr>", opts)
+keymap("n" ,"<leader>fg", ":Telescope live_grep<cr>", opts)
+keymap("n" ,"<leader>fb", ":Telescope buffers<cr>", opts)
+
+keymap("n" ,"<C-b>", ":NvimTreeToggle<cr>", opts)
+keymap("n" ,"<leader>r", ":NvimTreeRefresh<cr>", opts)
+keymap("n" ,"<leader>n", ":NvimTreeFindFile<cr>", opts)
