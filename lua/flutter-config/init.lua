a9a6967c63b8259143c4f93e8f6b2c4946da108e
Marc Trösken <marc.troesken@ratepay.com>
2022-07-05 08:45:39 +0200

    extend flutter conf

--- lua/flutter-config/init.lua
+++ lua/flutter-config/init.lua
@@ -1,5 +1,37 @@
 require("flutter-tools").setup({
+  ui = {
+    -- the border type to use for all floating windows, the same options/formats
+    -- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
+    border = "rounded",
+    -- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
+    -- please note that this option is eventually going to be deprecated and users will need to
+    -- depend on plugins like `nvim-notify` instead.
+    notification_style = 'native',
+  },
+
   widget_guides = {
     enabled = true,
+  },
+
+  outline = {
+    open_cmd = "30vnew", -- command to use to open the outline buffer
+    auto_open = false -- if true this will open the outline automatically when it is first populated
+  },
+
+  lsp = {
+    color = { -- show the derived colours for dart variables
+      enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+      background = false, -- highlight the background
+      foreground = false, -- highlight the foreground
+      virtual_text = true, -- show the highlight using virtual text
+      virtual_text_str = "■", -- the virtual text character to highlight
+    },
+
+  settings = {
+      showTodos = true,
+      completeFunctionCalls = true,
+      renameFilesWithClasses = "prompt", -- "always"
+      enableSnippets = true
+    }
   }
 })
